d7feff1efb9ad61ab623557b725e04e938aa9233
Adriano Rezende <atdrez@gmail.com>
2012-11-01 19:05:25 -0200

    Add remove method to vector

--- src/ctvector.h
+++ src/ctvector.h
@@ -6,7 +6,25 @@
 template <typename T>
 class CtVector : public std::vector<T>
 {
-
+public:
+    bool remove(const T &key);
 };
+
+
+template <typename T>
+inline bool CtVector<T>::remove(const T &key)
+{
+    typename CtVector<T>::iterator it;
+
+    for (it = CtVector<T>::begin(); it != CtVector<T>::end(); it++) {
+        if (*it == key) {
+            CtVector<T>::erase(it);
+            return true;
+        }
+    }
+
+    return false;
+}
+
 
 #endif
