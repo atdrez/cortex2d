c21c0e90b4c027b59df71c277a1207fb8dd38ce5
Adriano Rezende <atdrez@gmail.com>
2012-12-04 11:31:16 -0200

    Add contains method to list

--- src/ctlist.h
+++ src/ctlist.h
@@ -6,7 +6,21 @@
 template <typename T>
 class CtList : public std::list<T>
 {
-
+public:
+    bool contains(const T &value) const;
 };
+
+template <typename T>
+inline bool CtList<T>::contains(const T &value) const
+{
+    typename CtList<T>::const_iterator it;
+
+    for (it = CtList<T>::begin(); it != CtList<T>::end(); it++) {
+        if (*it == value)
+            return true;
+    }
+
+    return false;
+}
 
 #endif
